b8407555858ef2e2799281a37aef43f5ab3a1f06
undefinedVPhantom <141588113+undefinedVPhantom@users.noreply.github.com>
2023-09-23 02:52:12 +0300

    Update Script.lua

--- Loader/Script.lua
+++ Loader/Script.lua
@@ -28,25 +28,38 @@ local Window = Rayfield:CreateWindow({
     }
  })
 
- --LOCALS
- local startTime = os.clock()
- local execTime = ""
-
  --MAIN
  local Main = Window:CreateTab("Main", 11322155020)
+ local Section = Main:CreateSection("Special Message")
  local Paragraph = Main:CreateParagraph({Title = "Message from wasundefined", Content = "Thank you for using VPhantom. We appreciate it."})
- 
- local Label = Main:CreateLabel("")
 
- --LOOPS | DONT PUT ANY TYPE OF CODES UNDER THIS PLACE
- while true do
-    local currentTime = os.clock()
-    local elapsedTime = currentTime - startTime
-    execTime = "Elapsed time since the script executed | " .. string.format("%.0f", elapsedTime) .. " seconds."
+ --VERSIONS
+ local V = Window:CreateTab("Versions", 9692125126)
+ local Section = V:CreateSection("All VPhantom Versions")
+ local Button = V:CreateButton({
+    Name = "VPhantom | Free",
+    Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/undefinedVPhantom/VPhantom/main/Versions/Free'))
+    end,
+ })
+ local Button = V:CreateButton({
+    Name = "VPhantom | Premium",
+    Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/undefinedVPhantom/VPhantom/main/Versions/Premium'))
+    end,
+ })
+ local Button = V:CreateButton({
+    Name = "VPhantom | Admin",
+    Callback = function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/undefinedVPhantom/VPhantom/main/Versions/Admin'))
+    end,
+ })
+ local Section = V:CreateSection("Information About Versions")
+ local Paragraph = V:CreateParagraph({Title = "VPhantom | Free", Content = "Limited but great experience."})
+ local Paragraph = V:CreateParagraph({Title = "VPhantom | Premium", Content = "Limitless and magnificent experience with a massive script hub."})
+ local Paragraph = V:CreateParagraph({Title = "VPhantom | Admin", Content = "Hmm... I wonder whats inside."})
 
-
-    
-    Label:Set(execTime)
-
-    wait(1)
-end
+ --DISCORD
+ local Dis = Window:CreateTab("Discord", 8387379647)
+ local Section = Dis:CreateSection("Information About Discord")
+ local Paragraph = Dis:CreateParagraph({Title = "wasundefined's Discord", Content = "wasundefined, thats it. If you want to buy premium, dm me. "})
